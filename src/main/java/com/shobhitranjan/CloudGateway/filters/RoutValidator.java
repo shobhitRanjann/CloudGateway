07b5ea23e1c075485dd60e78a55bc7e8481a6bfd
git config --global user.name <shobhit.ranjan098@gmail.com>
2024-01-01 21:51:35 +0530

    update in routevalidator

--- src/main/java/com/shobhitranjan/CloudGateway/filters/RoutValidator.java
+++ src/main/java/com/shobhitranjan/CloudGateway/filters/RoutValidator.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RoutValidator {
     public static final List<String> openApiEndpoints = List.of("/api/v1/auth/signin","/api/v1/auth/signup","/api/v1/auth/refresh","/api/v1/auth/validate-token",
-            "/eureka","/product/all","/product/image/**","/product/pincode");
+            "/eureka","/product/all","/product/image/","/product/pincode");
 
     public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
             .noneMatch(uri -> request.getURI().getPath().contains(uri));
